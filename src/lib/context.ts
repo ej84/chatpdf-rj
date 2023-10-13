51b24507b154f8af2e14693acc10c8d1adfeb092
RjCoder <ej84@njit.edu>
2023-10-13 15:54:52 -0400

    Updated chat router and pages.

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -4,21 +4,18 @@ import { Pinecone } from "@pinecone-database/pinecone";
 import { convertToAscii } from "./utils";
 import { getEmbeddings } from "./embeddings";
 
-export const getPineconeClient = () => {
-    return new Pinecone({
-      apiKey: process.env.PINECONE_API_KEY!,
-      environment: process.env.PINECONE_ENVIRONMENT!
-    });
-};
-
 export async function getMatchesFromEmbeddings(embeddings: number[], fileKey: string) {
     try {
-        const index = await getPineconeClient().index('chatpdf-rj');
-        const namespace = index.namespace(convertToAscii(fileKey));
+        const client = new Pinecone({
+            environment: process.env.PINECONE_ENVIRONMENT!,
+            apiKey: process.env.PINECONE_API_KEY!,
+        });
+        const pineconeIndex = await client.index('chatpdf-rj');
+        const namespace = pineconeIndex.namespace(convertToAscii(fileKey));
         const queryResult = await namespace.query({
             topK: 5,
             vector: embeddings,
-            includeMetadata: true
+            includeMetadata: true,
         });
         return queryResult.matches || [];
     }
@@ -29,5 +26,20 @@ export async function getMatchesFromEmbeddings(embeddings: number[], fileKey: st
 }
 
 export async function getContext(query: string, fileKey: string) {
+    const queryEmbeddings = await getEmbeddings(query);
+    const matches = await getMatchesFromEmbeddings(queryEmbeddings, fileKey);
+    // check if queried embeddings are relevant.
+    const qualifyingDocs = matches.filter(
+        (match) => match.score && match.score > 0.7 // Relevant if match score is over 70%, Not relevant if it is less than 70%.
+    );
 
+    // Metadata Type
+    type Metadata = {
+        text: string,
+        pageNumber: number
+    };
+
+    let docs = qualifyingDocs.map(match => (match.metadata as Metadata).text);
+    // 5 vectors per page
+    return docs.join('\n').substring(0, 3000); // Limits characters up to 3000 to prevent overfeeding.
 }
